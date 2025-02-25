01244caa6193a1470c7cd732ba808918843fec46
Francesco Refolli <f.refolli@campus.unimib.it>
2025-02-25 19:10:10 +0100

    ok

--- tools/cityflow2sumo/README.md
+++ tools/cityflow2sumo/README.md
@@ -1,40 +1,40 @@
 # Conversion of RoadNet Json to SUMO NetXML
 
-# XML Road
+## XML Road
 
-```
+```xml
 <edge id="E8" from="J0" to="J16" priority="1" spreadType="center" shape="-20.87,48.49 -5.66,48.43">
   <lane id="E8_0" index="0" speed="13.89" length="15.21"/>
 </edge>
 ```
 
-# JSON Road
+## JSON Road
 
+```json
+{"id": "road_0_1_0",
+ "points": [{"x": -300, "y": 0},
+            {"x": 0, "y": 0}],
+ "lanes": [{"width": 3, "maxSpeed": 11.111},
+           {"width": 3, "maxSpeed": 11.111},
+           {"width": 3, "maxSpeed": 11.111}],
+ "startIntersection": "intersection_0_1",
+ "endIntersection": "intersection_1_1"}
 ```
-{'id': 'road_0_1_0',
- 'points': [{'x': -300, 'y': 0},
-            {'x': 0, 'y': 0}],
- 'lanes': [{'width': 3, 'maxSpeed': 11.111},
-           {'width': 3, 'maxSpeed': 11.111},
-           {'width': 3, 'maxSpeed': 11.111}],
- 'startIntersection': 'intersection_0_1',
- 'endIntersection': 'intersection_1_1'}
-```
 
-# XML Intersection
+## XML Intersection
 
-## Virtual := False
+### Virtual := False
 
-```
+```xml
 <junction id="J7" type="traffic_light" x="70.27" y="51.37" incLanes="E10_0 -E8_0" intLanes=":J7_0_0 :J7_1_0" >
   <request index="0" response="10" foes="10" cont="0"/>
   <request index="1" response="00" foes="01" cont="0"/>
 </junction>
 ```
 
-## Virtual := True
+### Virtual := True
 
-```
+```xml
 <junction id="J8" type="dead_end" x="39.07" y="-13.78" incLanes="-E11_0" intLanes=""/>
 
 <tlLogic id="J0" type="static" programID="0" offset="0">
@@ -44,3 +44,35 @@
     <phase duration="3"  state="yrGG"/>
 </tlLogic>
 ```
+
+# Conversion of RoadNet Json to SUMO NetXML
+
+## XML Route
+
+```xml
+<route id="r_0" edges="E3 E2.275"/>
+<vehicle id="v_0" depart="0.00" route="r_0"/>
+```
+
+## JSON Route
+
+```json
+{"vehicle": {"length": 5.0,
+             "width": 2.0,
+             "maxPosAcc": 2.0,
+             "maxNegAcc": 4.5,
+             "usualPosAcc": 2.0,
+             "usualNegAcc": 4.5,
+             "minGap": 2.5,
+             "maxSpeed": 11.111,
+             "headwayTime": 2},
+ "route": ["road_8_2_2", "road_7_2_3", "road_7_1_3"],
+ "interval": 1.0,
+ "startTime": 0,
+ "endTime": 0}
+```
+
+# Warnings
+
+- Always open and save the project with SUMO NetEdit before using it with the simulator to correct sorting errors.
+- Since CityFlow simulator supports discontinued routes but SUMO simulator don't, I thought about using the `routecheck.py` tool of SUMO but it simply remove the broken route leaving untouched the vehicles which used it. So I tried to deal with these broken routes.
--- tools/cityflow2sumo/__main__.py
+++ tools/cityflow2sumo/__main__.py
@@ -7,6 +7,12 @@ Converts RoadNet and FlowNet format json files to SUMO xml files
 from __future__ import annotations
 import json
 import math
+import hashlib
+import argparse
+import sys
+import os
+
+# UTILITY
 
 def load_network_json(path: str) -> dict:
   with open(path, "r") as file:
@@ -18,6 +24,8 @@ def load_routes_json(path: str) -> list:
 
 def indentation(indent: int) -> str:
   return "  " * indent
+
+# TYPES
 
 class Point:
   def __init__(self, x: float, y: float) -> None:
@@ -74,6 +82,12 @@ class Edge:
     ]
     footer = indentation(indent) + "</edge>"
     return "\n".join([header] + children + [footer])
+
+  def real_lane_index(self, lane_index: int) -> int:
+    """
+    Since SUMO is right-handed and CityFlow is left-handed i have to invert lane_index numbers in connections
+    """
+    return len(self.lanes) - 1 - lane_index
 
   def __repr__(self) -> str:
     return self.to_xml(0)
@@ -207,7 +221,7 @@ class TLLogic:
       phase.to_xml(indent + 1)
       for phase in self.phases
     ]
-    footer = "</tlLogic>"
+    footer = indentation(indent) + "</tlLogic>"
     return "\n".join([header] + children + [footer])
 
   def __repr__(self) -> str:
@@ -251,9 +265,9 @@ class Network:
     lines = []
     lines.append(indentation(indent) + '<?xml version="1.0" encoding="UTF-8"?>')
     lines.append(indentation(indent) + '<net version="1.20" junctionCornerDetail="5" limitTurnSpeed="5.50" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/net_file.xsd">')
-    for child in self.road_edges:
-      lines.append(child.to_xml(indent + 1))
     for child in self.junction_edges:
+      lines.append(child.to_xml(indent + 1))
+    for child in self.road_edges:
       lines.append(child.to_xml(indent + 1))
     for child in self.tllogics:
       lines.append(child.to_xml(indent + 1))
@@ -268,6 +282,86 @@ class Network:
 
   def __repr__(self) -> str:
     return self.to_xml(0)
+
+class Route:
+  def __init__(self, id: str, edges: list[str]) -> None:
+    self.id: str = id
+    self.edges: list[str] = edges
+
+  def to_xml(self, indent: int = 0) -> str:
+    return (
+      indentation(indent) + "<route id=\"%s\" edges=\"%s\"/>" % (
+        self.id, " ".join(self.edges)
+      )
+    )
+
+  @staticmethod
+  def name(route_index: int) -> str:
+    return "route_%s" % route_index
+
+  def __repr__(self) -> str:
+    return self.to_xml(0)
+
+class Vehicle:
+  def __init__(self, id: str, departure_time: float, route_id: str) -> None:
+    self.id: str = id
+    self.departure_time: float = departure_time
+    self.route_id: str = route_id
+
+  def to_xml(self, indent: int = 0) -> str:
+    return (
+      indentation(indent) + "<vehicle id=\"%s\" depart=\"%s\" route=\"%s\"/>" % (
+        self.id, self.departure_time, self.route_id
+      )
+    )
+
+  def __repr__(self) -> str:
+    return self.to_xml(0)
+
+  @staticmethod
+  def name(vehicle_index: int) -> str:
+    return "vehicle_%s" % vehicle_index
+
+class Routes:
+  def __init__(self, routes: list[Route], vehicles: list[Vehicle]) -> None:
+    self.routes: list[Route] = routes
+    self.vehicles: list[Vehicle] = vehicles
+
+  def to_xml(self, indent: int = 0) -> str:
+    lines = []
+    lines.append(indentation(indent) + '<?xml version="1.0" encoding="UTF-8"?>')
+    lines.append(indentation(indent) + '<routes xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/routes_file.xsd">')
+    for route in self.routes:
+      lines.append(route.to_xml(indent + 1))
+    for vehicle in self.vehicles:
+      lines.append(vehicle.to_xml(indent + 1))
+    lines.append('</routes>')
+    return "\n".join(lines)
+
+  def __repr__(self) -> str:
+    return self.to_xml(0)
+
+class Simulation:
+  def __init__(self, network: Network, routes: Routes) -> None:
+    self.network: Network = network
+    self.routes: Routes = routes
+
+  def to_xml(self, indent: int = 0) -> str:
+    lines = []
+    lines.append(indentation(indent) + '<?xml version="1.0" encoding="UTF-8"?>')
+    lines.append(indentation(indent) + '<configuration xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/sumoConfiguration.xsd">')
+    lines.append(indentation(indent) + '    <input>')
+    lines.append(indentation(indent) + '        <net-file value="network.net.xml"/>')
+    lines.append(indentation(indent) + '        <route-files value="routes.rou.xml"/>')
+    lines.append(indentation(indent) + '    </input>')
+    lines.append(indentation(indent) + '</configuration>')
+    return "\n".join(lines)
+
+  def __repr__(self) -> str:
+    return self.to_xml(0)
+
+
+# CONVERSION
 
 def translate_road(json_road: dict) -> Edge:
   points = [Point(p['x'], p['y']) for p in json_road['points']]
@@ -297,7 +391,7 @@ def translate_direction(json_direction: str) -> str:
   }
   return directions[json_direction]
 
-def translate_tl_intersection(json_intersection: dict) -> tuple[Junction, list[ViaConnection], list[InternalConnection], list[InternalEdge], TLLogic]:
+def translate_tl_intersection(json_intersection: dict, edge_map: dict[str, Edge]) -> tuple[Junction, list[ViaConnection], list[InternalConnection], list[InternalEdge], TLLogic]:
   junction_id = json_intersection['id']
   incoming_lanes = {}
   into_lanes = {}
@@ -318,8 +412,8 @@ def translate_tl_intersection(json_intersection: dict) -> tuple[Junction, list[V
       edge_name = InternalEdge.name(junction_id, index)
       lane_name = Lane.name(edge_name, 0)
       
-      via_connection = ViaConnection(from_edge, to_edge, from_lane, to_lane, direction, index, lane_name, junction_id)
-      internal_connection = InternalConnection(edge_name, to_edge, 0, to_lane, direction)
+      via_connection = ViaConnection(from_edge, to_edge, edge_map[from_edge].real_lane_index(from_lane), edge_map[to_edge].real_lane_index(to_lane), direction, index, lane_name, junction_id)
+      internal_connection = InternalConnection(edge_name, to_edge, 0, edge_map[to_edge].real_lane_index(to_lane), direction)
       junction_lane = Lane(id=lane_name, index=0, speed=3.93, length=2.19)
       junction_edge = InternalEdge(id=edge_name, lanes=[junction_lane])
 
@@ -332,14 +426,19 @@ def translate_tl_intersection(json_intersection: dict) -> tuple[Junction, list[V
   n_of_via_connections = len(via_connections)
 
   phases: list[Phase] = []
+  previous_phase_state: str|None = None
   for tl_phase in json_intersection['trafficLight']['lightphases']:
     gyr_map = {c:'r' for c in range(n_of_via_connections)}
     for greenRoadLink_index in tl_phase["availableRoadLinks"]:
       for greenLaneLink_index in roadToLaneLinks[greenRoadLink_index]:
-        gyr_map[greenLaneLink_index] = 'G'
+        if previous_phase_state is None or previous_phase_state[greenLaneLink_index] == 'G':
+          gyr_map[greenLaneLink_index] = 'G'
+        else:
+          gyr_map[greenLaneLink_index] = 'g'
     phase_state = "".join([gyr_map[c] for c in range(n_of_via_connections)])
     phase_duration = tl_phase["time"]
     phases.append(Phase(duration=phase_duration, state=phase_state))
+    previous_phase_state = phase_state
   tllogic = TLLogic(id=junction_id, phases=phases)
 
   junction = Junction(
@@ -369,6 +468,9 @@ def translate_virtual_intersection(json_intersection: dict, incoming_map: dict[s
 
   return (junction,)
 
+def map_of_edges(edges: list[Edge]) -> dict[str, Edge]:
+  return {edge.id:edge for edge in edges}
+
 def map_incoming_into_junction_edges(edges: list[Edge]) -> tuple[dict[str, list[str]], dict[str, list[str]]]:
   raw_incoming_map: dict[str, dict[str, int]] = {}
   raw_into_map: dict[str, dict[str, int]] = {}
@@ -387,11 +489,21 @@ def map_incoming_into_junction_edges(edges: list[Edge]) -> tuple[dict[str, list[
 
   return incoming_map, into_map
 
-def translate_network(json_network: dict):
+def map_of_adiacency_of_edges(via_connections: list[ViaConnection]) -> dict[str, dict[str, bool]]:
+  adiacency: dict[str, dict[str, bool]] = {}
+  for via_connection in via_connections:
+    source, target = via_connection.from_edge, via_connection.to_edge
+    if source not in adiacency:
+      adiacency[source] = {}
+    adiacency[source][target] = True
+  return adiacency
+
+def translate_network(json_network: dict) -> Network:
   json_roads = json_network['roads']
   json_intersections = json_network['intersections']
 
   road_edges = [translate_road(json_road) for json_road in json_roads]
+  edge_map = map_of_edges(road_edges)
   junction_incoming_map, junction_into_map = map_incoming_into_junction_edges(road_edges)
 
   junctions = []
@@ -404,7 +516,7 @@ def translate_network(json_network: dict):
       _junction, = translate_virtual_intersection(json_intersection, junction_incoming_map, junction_into_map)
       junctions.append(_junction)
     else:
-      _junction, _via_connections, _internal_connections, _junction_edges, tllogic = translate_tl_intersection(json_intersection)
+      _junction, _via_connections, _internal_connections, _junction_edges, tllogic = translate_tl_intersection(json_intersection, edge_map)
       junctions.append(_junction)
       internal_connections += _internal_connections
       via_connections += _via_connections
@@ -413,11 +525,72 @@ def translate_network(json_network: dict):
 
   return Network(road_edges, junctions, via_connections, internal_connections, junction_edges, tllogics)
 
+def valid_route(route: list[str], adiacency_map: dict[str, dict[str, bool]]) -> bool:
+  for i in range(1, len(route)):
+    source, target = route[i - 1], route[i]
+    if source not in adiacency_map or target not in adiacency_map[source] or not adiacency_map[source][target]:
+      return False
+  return True
+
+def fix_route(route: list[str], adiacency_map: dict[str, dict[str, bool]]) -> None:
+  pass
+
+def translate_routes(json_routes: list, network: Network) -> Routes:
+  adiacency_map: dict[str, dict[str, bool]] = map_of_adiacency_of_edges(network.via_connections)
+  # print(json.dumps(adiacency_map))
+
+  raw_routes: dict[str, Route] = {}
+  route_validity: dict[str, bool] = {}
+  vehicles: list[Vehicle] = []
+
+  for json_route in json_routes:
+    route_hash = hashlib.sha256("/".join(json_route['route']).encode()).digest().hex()
+    if route_hash in route_validity and not route_validity[route_hash]:
+      print("WARNING", "Skipping vehicle", json_route, "since it uses the reclaimed route", route_hash)
+      continue
+    if route_hash not in raw_routes:
+      # Check Route
+      edges = json_route['route']
+      if not valid_route(edges, adiacency_map):
+        route_validity[route_hash] = False
+        print("WARNING", "Skipping route", edges, "since it is broken")
+        continue
+      # Add Route
+      route_index = len(raw_routes)
+      route_id = Route.name(route_index)
+      route = Route(id=route_id, edges=edges)
+      raw_routes[route_hash] = route
+      route_validity[route_hash] = True
+    route = raw_routes[route_hash]
+    # Add Vehicle
+    vehicle_index = len(vehicles)
+    vehicle_id = Vehicle.name(vehicle_index)
+    vehicle = Vehicle(id=vehicle_id, departure_time=json_route['startTime'], route_id=route.id)
+    vehicles.append(vehicle)
+
+  routes = list(raw_routes.values())
+  return Routes(routes=routes, vehicles=vehicles)
+
 if __name__ == "__main__":
-  json_network = load_network_json("../../assets/tools/cityflow2sumo/roadnet_28_7.json")
-  json_routes = load_routes_json("../../assets/tools/cityflow2sumo/anon_28_7_newyork_real_double.json")
+  argument_parser = argparse.ArgumentParser("Cityflow2SUMO", description="Converts CityFlow RoadNet/FlowNet format to SUMO XML files")
+  argument_parser.add_argument("network_file", type=str, help="Input network file in JSON CityFlow format")
+  argument_parser.add_argument("routes_file", type=str, help="Input routes file in JSON CityFlow format")
+  argument_parser.add_argument("-o", "--output", type=str, default="./output", help="Output directory for SUMO project")
+  cli_args = argument_parser.parse_args(sys.argv[1:])
+
+  json_network = load_network_json(cli_args.network_file)
+  json_routes = load_routes_json(cli_args.routes_file)
 
   network: Network = translate_network(json_network)
+  routes: Routes = translate_routes(json_routes, network)
+  simulation: Simulation = Simulation(network, routes)
 
-  with open("output.net.xml", "w") as file:
+  if not os.path.exists(cli_args.output):
+    os.makedirs(cli_args.output)
+
+  with open("%s/network.net.xml" % (cli_args.output,), "w") as file:
     file.write(network.to_xml())
+  with open("%s/routes.rou.xml" % (cli_args.output,), "w") as file:
+    file.write(routes.to_xml())
+  with open("%s/simulation.sumocfg" % (cli_args.output,), "w") as file:
+    file.write(simulation.to_xml())
